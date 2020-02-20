f3fa3d7a534f730b86f97d3a1cdc3b306d4d4c81
ahmedhabeeb73 <az73system@gmail.com>
2020-02-20 23:12:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var
+{
+    class Program
+    {
+        private const string V = "ahmed";
+
+        static void Main(string[] args)
+        {
+            string name = V;
+            Console.WriteLine(name);
+        }
+    }
+}
